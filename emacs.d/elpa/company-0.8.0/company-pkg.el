a7b135aac87090a0e60af29bc07e869609bf1df6
brettlangdon <brett@blangdon.com>
2014-06-14 08:17:47 -0400

    add company

---
+++ emacs.d/elpa/company-0.8.0/company-pkg.el
@@ -0,0 +1,1 @@
+(define-package "company" "0.8.0" "Modular text completion framework" '((emacs "24.1") (cl-lib "0.5")) :keywords ("abbrev" "convenience" "matching") :url "http://company-mode.github.io/")
