43c740b2a8ca042d67707fc029edebdc1aef8b45
brettlangdon <brett@blangdon.com>
2015-05-08 09:20:51 -0400

    remove some files

--- emacs.d/elpa/color-theme-monokai-0.0.5/color-theme-monokai-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; color-theme-monokai-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "color-theme-monokai" "../../../../.emacs.d/elpa/color-theme-monokai-0.0.5/color-theme-monokai.el"
-;;;;;;  "5e588ec55086e698d854b204864f3032")
-;;; Generated autoloads from ../../../../.emacs.d/elpa/color-theme-monokai-0.0.5/color-theme-monokai.el
-
-(autoload 'color-theme-monokai "color-theme-monokai" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/color-theme-monokai-0.0.5/color-theme-monokai-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/color-theme-monokai-0.0.5/color-theme-monokai.el")
-;;;;;;  (21569 47878 908014 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; color-theme-monokai-autoloads.el ends here
