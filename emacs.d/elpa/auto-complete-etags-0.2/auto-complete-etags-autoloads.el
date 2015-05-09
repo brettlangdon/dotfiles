858942279a49e796a8d6286bf4fa7848306eef19
brettlangdon <brett@blangdon.com>
2015-05-08 21:21:22 -0400

    upgrade packages

--- emacs.d/elpa/auto-complete-etags-0.2/auto-complete-etags-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; auto-complete-etags-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("auto-complete-etags.el") (21570 25620
-;;;;;;  582723 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; auto-complete-etags-autoloads.el ends here
---
+++ emacs.d/elpa/centered-cursor-mode-20150420.1942/centered-cursor-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "centered-cursor-mode" "20150420.1942" "cursor stays vertically centered" 'nil :url "http://www.emacswiki.org/emacs/centered-cursor-mode.el" :keywords '("convenience"))
