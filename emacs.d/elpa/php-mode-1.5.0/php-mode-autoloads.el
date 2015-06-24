a9cae4c268db282aded389dc9aefaaa41f8785c7
brettlangdon <brett@blangdon.com>
2015-06-24 10:18:18 -0400

    upgrade emacs packages

--- emacs.d/elpa/php-mode-1.5.0/php-mode-autoloads.el
+++
@@ -1,35 +0,0 @@
-;;; php-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "php-mode" "../../../../.emacs.d/elpa/php-mode-1.5.0/php-mode.el"
-;;;;;;  "025ffb038ae62499cd2aa77dc6941e9c")
-;;; Generated autoloads from ../../../../.emacs.d/elpa/php-mode-1.5.0/php-mode.el
-
-(defvar php-file-patterns '("\\.php[s34]?\\'" "\\.phtml\\'" "\\.inc\\'") "\
-List of file patterns for which to automatically invoke `php-mode'.")
-
-(custom-autoload 'php-file-patterns "php-mode" nil)
-
-(autoload 'php-mode "php-mode" "\
-Major mode for editing PHP code.
-
-\\{php-mode-map}
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/php-mode-1.5.0/php-mode-autoloads.el"
-;;;;;;  "../../../../.emacs.d/elpa/php-mode-1.5.0/php-mode.el") (21570
-;;;;;;  24357 678941 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; php-mode-autoloads.el ends here
