5be2317806e0e40fbbd276004af2bd8a2a8081a7
brettlangdon <brett@blangdon.com>
2016-01-17 10:44:35 -0500

    upgrade emacs packages

--- emacs.d/elpa/centered-cursor-mode-20150420.1942/centered-cursor-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "centered-cursor-mode" "20150420.1942" "cursor stays vertically centered" 'nil :url "http://www.emacswiki.org/emacs/centered-cursor-mode.el" :keywords '("convenience"))
