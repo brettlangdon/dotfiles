77d9d8c09ea3c167b7b0018744ac52d338e6510f
brettlangdon <me@brett.is>
2018-06-01 08:18:30 -0400

    Update dotfile

--- zshrc.d/bootstrap.zsh
+++ zshrc.d/bootstrap.zsh
@@ -10,6 +10,10 @@ source ${ZSH_DIR}/.env.zsh
 # Load in oh-my-zsh
 source ${ZSH}/oh-my-zsh.sh
 
-# Load virtualenvwrapper if it is exists
+# Load virtualenvwrapper if it exists
 # DEV: Load the lazy script for faster load times
 test -x /usr/local/bin/virtualenvwrapper_lazy.sh && source /usr/local/bin/virtualenvwrapper_lazy.sh
+
+# Load zsh-syntax-highlighting if it exists
+# DEV: brew install zsh-syntax-highlighting
+test -f /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh && source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
--- zshrc.d/lib/aliases.sh
+++ zshrc.d/lib/aliases.sh
@@ -1,4 +1,7 @@
 alias up="vagrant up && vagrant ssh"
 alias stayawake="caffeinate -s -d -i"
 alias emacs="emacs -nw"
-alias docker-login="aws ecr get-login --region us-east-1 | sh"
+alias docker-login="aws ecr get-login --no-include-email --region us-east-1 | sh"
+alias docker-start="open --background /Applications/Docker.app"
+alias clear-dns="sudo dscacheutil -flushcache || sudo killall -HUP mDNSResponder"
+alias docker-top='watch -n 1 "clear && docker top $1"'
