53141b5d542de834321a5deccf9f88ee6b9ee997
brettlangdon <me@brett.is>
2018-12-30 13:01:33 -0500

    consolidate all core into init.el

--- emacs.d/funcs.el
+++
@@ -1,9 +0,0 @@
-(defun enable-flycheck (hook)
-  (add-hook hook
-	    ;; Flycheck
-	    ;; http://www.flycheck.org/en/latest/index.html
-	    `(lambda ()
-	       (use-package flycheck
-		 :diminish flycheck-mode
-		 :config
-		 (flycheck-mode)))))
