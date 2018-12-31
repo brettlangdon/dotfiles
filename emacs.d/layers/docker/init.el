f533746c54a9e279f1817f69c501bdc9a3d64009
brettlangdon <brett.langdon@datadoghq.com>
2018-12-31 13:26:33 -0500

    add docker layer

---
+++ emacs.d/layers/docker/init.el
@@ -0,0 +1,12 @@
+;; Dockerfile mode
+;; https://github.com/spotify/dockerfile-mode
+(use-package dockerfile-mode
+  :mode ("Dockerfile\\'" . dockerfile-mode)
+  :diminish dockerfile-mode)
+
+;; docker-compose mode
+;; https://github.com/meqif/docker-compose-mode
+(use-package docker-compose-mode
+  ;; DEV: https://github.com/meqif/docker-compose-mode/blob/c9f131d2c90d652435d407fd36c40feebfed1dad/docker-compose-mode.el#L164-L165
+  :mode ("docker-compose[^/]*\\.yml\\'" . docker-compose-mode)
+  :diminish docker-compose-mode)
