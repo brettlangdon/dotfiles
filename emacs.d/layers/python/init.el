008c2820621ca26c5428590333a8e241dec3f13a
brettlangdon <brett.langdon@datadoghq.com>
2019-01-18 18:10:55 -0500

    work on python layer

--- emacs.d/layers/python/init.el
+++ emacs.d/layers/python/init.el
@@ -2,27 +2,24 @@
 ;; https://github.com/proofit404/anaconda-mode
 (use-package anaconda-mode
   :diminish anaconda-mode
-  :requires eldoc
-  :after eldoc
-  :init
-  (add-hook 'python-hook #'anaconda-mode)
-  (add-hook 'python-hook #'anaconda-eldoc-mode))
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook 'anaconda-mode)
+  (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
 
 ;; Company anaconda
 ;; https://github.com/proofit404/company-anaconda
 (use-package company-anaconda
   :mode ("\\.py" . python-mode)
   :after (company anaconda-mode)
-  :init
-  (add-hook 'python-hook
-            (eval-after-load "company"
-              '(add-to-list 'company-backends 'company-anaconda))))
+  :config
+  (add-to-list 'company-backends 'company-anaconda))
 
 ;; Enable eldoc mode
 (use-package eldoc
   :diminish eldoc-mode
-  :init
-  (add-hook 'python-mode #'eldoc-mode))
+  :config
+  (add-hook 'python-mode 'eldoc-mode))
 
 ;; Enable pip-requirements when editing a `requirements.txt` file
 (use-package pip-requirements
